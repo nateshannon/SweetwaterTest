e0fede36e55c87d02caddbe249c8365e259089f4
carpe <carpe@10.0.0.135>
2021-08-02 01:19:47 -0400

    Common header, stylesheet added

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+            crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+            crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/7df19bc93b.js" crossorigin="anonymous"></script>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Cinzel+Decorative:wght@400;700;900&family=Cutive&family=Fontdiner+Swanky&family=Fredoka+One&family=Lancelot&family=Lato:wght@300;400;700;900&family=Libre+Barcode+128+Text&family=Libre+Barcode+39+Text&family=Libre+Barcode+EAN13+Text&family=Montserrat+Subrayada:wght@400;700&family=Montserrat:wght@300;400;700;900&family=Mr+Dafoe&family=Oswald:wght@200;400;700&family=Pattaya&family=Permanent+Marker&family=Solway:wght@700;800&display=swap" rel="stylesheet">
+        <link href="content/sweetwater.css" rel="stylesheet">
+        
+        <meta charset="UTF-8">
+        <title><?=$pageTitle; ?></title>
+    </head>
+    <body>
--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <?php
+$pageTitle = "Sweetwater Comment Post-Processor";
 
 require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/conn.php');
-
+require_once(__DIR__ . '/header.php');
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+    <div class="px-4 py-4 my-4 text-center">
+        <div class="row">
+            <div class="col-lg-3">     
+                
+            </div>
+            <div class="col-lg-6 text-center">
+
+                <h1 class="subtleShadow" style="text-align: left; font-weight: 700; font-family: 'Oswald', sans-serif;"><span class="sweetwaterLogoType">Sweetwater</span> Order Comments</h1>
+                
+                <h4 class="" style="text-align: left; font-weight: 400; font-family: 'Montserrat', sans-serif;">Body copy</h4>
+                
+                <h4 class="" style="text-align: left; font-weight: 400; font-family: 'Permanent Marker', cursive;">Accent copy</h4>                
+                
+                
+                
+
+            </div>
+            <div class="col-lg-3">
+                
+            </div>
+        </div>
+    </div>
+        
         <?php
         
         $result = mysqli_query($conn, "SELECT * FROM sweetwater_test WHERE orderid=25006706 LIMIT 1");
@@ -20,5 +37,9 @@ require_once(__DIR__ . '/conn.php');
         
         ?>
         
-    </body>
-</html>
+    
+    
+
+<?php
+require_once(__DIR__ . '/footer.php');
+?>
