5b044e43136a7bed9375983bd29bd8f7708a5f2e
carpe <carpe@10.0.0.135>
2021-08-08 03:54:43 -0400

    Name detection

--- metadata-generator.php
+++ metadata-generator.php
@@ -95,9 +95,9 @@ echo "<hr>";
 //positive = require, include
 //negative = waive, leave, left, no, remove, without
 
-$sigsubjectKeywords = array("signature", "sig", "sign", "door", "package");
+$sigsubjectKeywords = array("signature", "sig", "sign", "door", "package", "porch");
 $sigpositiveKeywords = array("include");
-$signegativeKeywords = array("waive", "leave", "left", "remove", "no", "without");
+$signegativeKeywords = array("waive", "leave", "left", "remove", "not", "no", "without");
 $sigpositive = 0;
 for ($x = 0; $x < count($row_sentences); $x++) {    
     $sigpositive += subject_decision($row_sentences[$x], $sigsubjectKeywords, $sigpositiveKeywords, $signegativeKeywords);
@@ -113,33 +113,35 @@ echo "<hr>";
 
 
 
-echo "<br>meta: " . metaphone_search('Bit o Honey', $row_comments);
 
+
+
+//split comment into words
+  //find sounds like candy words from candy table
+    //create candy association
+echo "<br>meta: " . metaphone_search('Bit o Honey', $row_comments);
 echo "<br>exact: " . exact_search('Bit O Honey', $row_comments);
 
-
-
-
-$candy_sql = "SELECT name FROM candy ORDER BY name ASC";
+$candy_sql = "SELECT id, name FROM candy ORDER BY name ASC";
 $candies = array();
 $candy_result = mysqli_query($conn, $candy_sql);
 if ($candy_result->num_rows > 0) {
     while($candy_row = $candy_result->fetch_assoc()) {
-        $candies[] = $candy_row["name"];
+        $candies[] = array("id" => $candy_row["id"], "name" => $candy_row["name"]);
     }
 }         
 
 for ($c = 0; $c < count($candies); $c++) {
-    if (exact_search($candies[$c], $row_comments)) {
+    if (exact_search($candies[$c]['name'], $row_comments)) {
         // exact match found
-        echo $candies[$c] . " located exactly.<br>";
-        // CREATE ASSOCIATION
+        echo $candies[$c]['name'] . " located exactly.<br>";
+        // CREATE ASSOCIATION orderid <-> candyid
     } else {
         // exact match NOT found
-        if (metaphone_search($candies[$c], $row_comments)) {
+        if (metaphone_search($candies[$c]['name'], $row_comments)) {
             // metaphone match found
-            echo $candies[$c] . " located by metaphone.<br>";
-            // CREATE ASSOCIATION
+            echo $candies[$c]['name'] . " located by metaphone.<br>";
+            // CREATE ASSOCIATION orderid <-> candyid
         } else {
             // no match found
         }
@@ -149,9 +151,142 @@ for ($c = 0; $c < count($candies); $c++) {
 
 
 
-//split comment into words
-  //find sounds like candy words from candy table
-    //create candy association
+
+
+
+
+
+
+
+
+
+echo "<hr>Names Found:<br>";
+
+
+// DECENT!!!
+//preg_match_all('/[A-Z][a-z]+\s+(?:[A-Z][a-z]*\.?\s*)?[A-Z][a-z]*/', $row_comments, $nameMatches, PREG_OFFSET_CAPTURE);
+
+//OK
+//preg_match_all('/(?:\b[A-Z][a-z]*\W+)*[A-Z][a-z]*\b/', $row_comments, $nameMatches, PREG_OFFSET_CAPTURE);
+
+
+
+// FIND NAMES NOT IN SYSTEM
+preg_match_all('/[A-Z][a-z\.]+\s+(?:[A-Z][a-z\.\-]*\s*)?[A-Z][A-Za-z\.\-]*/', $row_comments, $nameMatches, PREG_OFFSET_CAPTURE);
+
+if (isset($nameMatches[0])) {
+    
+    for ($m = 0; $m < count($nameMatches[0]); $m++) {
+        $matchedName = $nameMatches[0][$m][0];
+        $matchedName = exclude_words($matchedName);
+        //$matchedName = $matchedName . "....";
+        
+        if ($matchedName != "") {
+            if ($nameMatches[0][$m][0] != $matchedName) {
+                $temp = preg_replace("/(^\W*|\W*$)/", "", $matchedName);
+                $matchedName = $temp;
+            }
+            //echo "name: " . $nameMatches[0][$m][0] . " => " . $matchedName . ";<br>";
+            
+
+            
+            $personId = ensure_person_exists($matchedName, TRUE, TRUE, $conn); // check against system
+            if ($personId == -1) {
+                $personId = ensure_person_exists($matchedName, TRUE, FALSE, $conn); // check against auto-gen
+            }
+            echo "<br>Person Id: " . $personId . "<br>";
+            
+            
+            
+        }       
+        
+    }
+    
+}
+echo "<hr>";
+
+//echo "<br>Person Id 2: " . ensure_person_exists("Mark Smith", TRUE, FALSE, $conn) . "<br>";
+
+
+
+
+
+
+
+
+
+
+// sales
+//30814225
+//30823762
+//30261410
+//30812036
+//
+// referral
+//30823762
+//30596166
+//30820472
+
+
+echo "<hr>";
+
+
+
+$people_sql = "SELECT id, name FROM people ORDER BY name ASC";
+$people = array();
+$people_result = mysqli_query($conn, $people_sql);
+if ($people_result->num_rows > 0) {
+    while($people_row = $people_result->fetch_assoc()) {
+        $people[] = array("id" => $people_row["id"], "name" => $people_row["name"]);
+    }
+}    
+
+for ($c = 0; $c < count($people); $c++) {
+    $found_person = "";
+    if (exact_search($people[$c]['name'], $row_comments)) {
+        // exact match found
+        $found_person = $people[$c]['name'];
+        echo "<br>" . $found_person . " located exactly.<br>";
+        // Look for referral or sales
+        // CREATE ASSOCIATION orderid <-> personid
+    } else {
+        // exact match NOT found
+        if (metaphone_search($people[$c]['name'], $row_comments)) {
+            // metaphone match found
+            $found_person = $people[$c]['name'];
+            echo "<br>" . $found_person . " located by metaphone.<br>";
+            // Look for referral or sales
+            // CREATE ASSOCIATION orderid <-> personid
+        } else {
+            // no match found
+        }
+    }
+    
+    if ($found_person != "") {
+        echo "<hr>Refer Distance: ";
+        $temp = person_subject_assoc_distance($row_comments, $found_person, array("refer", "recommend", "told", "heard", "swears by"));
+        echo $temp['distance'] . " (" . $temp['keyword'] . ")";
+
+        echo "<hr>";
+        
+        echo "<hr>Sales Distance: ";
+        $temp = person_subject_assoc_distance($row_comments, $found_person, array("sales", "engineer", "rep", "credit", "commission", "thank you", "attn", "help", "client"));
+        echo $temp['distance'] . " (" . $temp['keyword'] . ")";
+    }
+//    $referral_subject_keywords = array("signature", "sig", "sign", "door", "package");
+//    $referral_positive_keywords = array("include");
+//    $referral_negative_keywords = array("waive", "leave", "left", "remove", "no", "without");
+//    $referral_positive = 0;
+//    for ($x = 0; $x < count($row_sentences); $x++) {    
+//        $sigpositive += subject_decision($row_sentences[$x], $sigsubjectKeywords, $sigpositiveKeywords, $signegativeKeywords);
+//
+//    }
+    
+    
+}
+
+
+
 
 
 
@@ -163,9 +298,9 @@ for ($c = 0; $c < count($candies); $c++) {
   //make associations for found known names
     //use referal and sales keywords to record association type
   //use sounds like for fuzzy matches
-  //use regex to identify likely names
-    //create person
-      //create association and type
+  //!use regex to identify likely names
+    //!create person
+      //!create association and type
 
 // if name is found in same sentence as refer or recommend or heard or told, it is a referral
 // if name is in same sentence as sales rep or sales engineer or credit or commission or "thank you" or attn, it is sales
