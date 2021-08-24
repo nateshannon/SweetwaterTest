e3ee2bf53baa33ab85c30f1a2895d039b7acb593
carpe <carpe@10.0.0.135>
2021-08-23 22:03:48 -0400

    Invalid db config handling

--- conn.php
+++ conn.php
@@ -1,11 +1,5 @@
 <?php
 
-$conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
 
 $showMenu = FALSE;
 $databaseTablesSweetwaterTest = FALSE;
@@ -14,44 +8,72 @@ $databaseTablesCandyAssoc = FALSE;
 $databaseTablesPeople = FALSE;
 $databaseTablesPeopleAssoc = FALSE;
 
-if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'sweetwater_test'")) {
-    if($result->num_rows == 1) {
-        $databaseTablesSweetwaterTest = TRUE;
+
+if ($connect = mysqli_connect($dbServerName, $dbUsername, $dbPassword)) {
+    $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
+    
+    if ($conn->connect_error) {
+      //die("Connection failed: " . $conn->connect_error);
+    } else {
+        if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'sweetwater_test'")) {
+            if($result->num_rows == 1) {
+                $databaseTablesSweetwaterTest = TRUE;
+            }
+        }
+        if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'candy'")) {
+            if($result->num_rows == 1) {
+                $databaseTablesCandy = TRUE;
+            }
+        }
+        if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'candy_assoc'")) {
+            if($result->num_rows == 1) {
+                $databaseTablesCandyAssoc = TRUE;
+            }
+        }
+        if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'people'")) {
+            if($result->num_rows == 1) {
+                $databaseTablesPeople = TRUE;
+            }
+        }
+        if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'people_assoc'")) {
+            if($result->num_rows == 1) {
+                $databaseTablesPeopleAssoc = TRUE;
+            }
+        }
+
+        if ($databaseTablesSweetwaterTest && 
+            $databaseTablesCandy &&
+            $databaseTablesCandyAssoc &&
+            $databaseTablesPeople &&
+            $databaseTablesPeopleAssoc) {
+            $showMenu = TRUE;
+        } else {
+            $currentPage = basename($_SERVER["REQUEST_URI"], ".php");
+            if ($currentPage != "index") {
+                header('Location: index.php');
+                exit;
+            }
+        }
     }
-}
-if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'candy'")) {
-    if($result->num_rows == 1) {
-        $databaseTablesCandy = TRUE;
-    }
-}
-if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'candy_assoc'")) {
-    if($result->num_rows == 1) {
-        $databaseTablesCandyAssoc = TRUE;
-    }
-}
-if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'people'")) {
-    if($result->num_rows == 1) {
-        $databaseTablesPeople = TRUE;
-    }
-}
-if ($result = mysqli_query($conn, "SHOW TABLES LIKE 'people_assoc'")) {
-    if($result->num_rows == 1) {
-        $databaseTablesPeopleAssoc = TRUE;
-    }
+    
+    
+    
+} else {
+    //die("Unable to Connect to '$dbServerName'");
 }
 
-if ($databaseTablesSweetwaterTest && 
-    $databaseTablesCandy &&
-    $databaseTablesCandyAssoc &&
-    $databaseTablesPeople &&
-    $databaseTablesPeopleAssoc) {
-    $showMenu = TRUE;
-} else {
-    $currentPage = basename($_SERVER["REQUEST_URI"], ".php");
-    if ($currentPage != "index") {
-        header('Location: index.php');
-        exit;
-    }
-}
+
+
+//mysqli_select_db($dbName) or die("Could not openxxx the db '$dbName'");
+
+
+//die();
+
+
+
+
+
+
+
 
 ?>
